6df676fb44bbef0df6b3811db28bfbcbee805d13
Pavanarathod <edrocullen13@gmail.com>
2022-08-26 22:20:40 +0530

    Basic data types

---
+++ DataTypes/data.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    char middle_name = 'k';
+    cout << "My middle name is " << middle_name << endl;
+
+    unsigned short int exam_score{65};
+    cout << "My exam score is " << exam_score << endl;
+
+    int countries_rep{65};
+    cout << "There were about " << countries_rep << endl;
+
+    long people_in_florida{435634534};
+    cout << "There were about " << people_in_florida << endl;
+
+    long people_in_earth{7600000};
+    cout << "There were about " << people_in_earth << endl;
+
+    // Floting point number.
+
+    float cat_payment{401.23};
+    cout << cat_payment << endl;
+
+    double pi{3.14159};
+    cout << pi << endl;
+
+    // Boolean
+
+    bool game_over{false};
+    cout << game_over << endl;
+
+    return 0;
+}
